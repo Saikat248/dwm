e63bb43e58420c3051984e1a07edd73dce49437c
Saikat248 <saikat403@gmail.com>
2020-09-25 18:56:10 +0530

    first buils in main machine

--- config.def.h
+++ config.def.h
@@ -36,9 +36,15 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                         instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",                          NULL,       NULL,       0,            1,           -1 },
+	{ "Brave-browser",                 NULL,       NULL,       1 << 8,       0,           -1 },  
+	{ "Slack",                         NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Microsoft Teams - Preview",     NULL,       NULL,       1 << 6,       0,           -1 },
+	{ NULL,                            NULL,      "MOLDEN",    0,            1,           -1 },
+	{ "Avogadro",                      NULL,       NULL,       0,            1,           -1 },
+	{ "Nemo",                          NULL,       NULL,       0,            1,           -1 },
+	{ "Scribus",                       NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -68,13 +74,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
-static const char *web[] = {"firefox", NULL };
+static const char *web[] = {"brave-browser", NULL };
+static const char *slack[] = {"slack", NULL };
+static const char *MSteam[] = {"teams", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = web } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = slack } },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = MSteam } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
